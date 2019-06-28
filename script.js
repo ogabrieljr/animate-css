d953e8b55da0bfc24221f5db91521572480e3731
whatistheplot <47022817+whatistheplot@users.noreply.github.com>
2019-06-28 12:24:35 -0300

    update js

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-function createParagraph(text, className, animation, animated){
-  var p = document.createElement("p");
-  p.innerText = text;
-  if(className){
-      p.classList.add(className, animation, animated)
-  }
-  return p;
+function createParagraph(text, className, animated, animation) {
+	let p = document.createElement("p");
+	p.innerText = text;
+	if (className) p.classList.add(className, animation, animated);
+	return p;
 }
-window.addEventListener("keypress", function (e) {
-  var oldP = document.querySelector(".letter");
-  if(oldP !== null){
-      oldP.parentElement.removeChild(oldP)
-  }
-  var p = createParagraph(e.key, "letter", "animated", "flip");
-  document.body.append(p);
-})
+window.addEventListener("keypress", function(e) {
+  let oldP = document.querySelector(".letter");
+	if (oldP) oldP.parentElement.removeChild(oldP);
+	let p = createParagraph(e.key, "letter", "animated", "flip");
+	document.body.append(p);
+});
